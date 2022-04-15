a6909874e59b0702626339c0f94e68b7c0c71f1f
Iamultrna King <85116654+Iamultrna-King@users.noreply.github.com>
2022-04-15 19:03:03 +0530

    C115

--- main.js
+++ main.js
@@ -3,10 +3,31 @@ function preload() {}
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function draw() {}
+function modelLoaded() {
+    console.log("PoseNet model loaded");
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+}
 
 function take_snapshot() {
     save("my_picture.png");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X : " + noseX);
+        console.log("Nose Y : " + noseY); 
+    }
 }
